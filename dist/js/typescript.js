c9ad28e6591e22223a68cf54711d04b2680a55d8
Salomao <salomao.machado.75@gmail.com>
2023-08-29 22:39:19 -0300

    revendo o basico sobre typescript

---
+++ dist/js/typescript.js
@@ -0,0 +1,9 @@
+// Tipos Primitivos
+let valor = 3000;
+let nome = "";
+let isPago = false;
+let qualquer = 22;
+qualquer = "";
+// Arrays
+const lista = [];
+lista.push("miu", "dog", 22, true, []);
---
+++ src/typescript.ts
@@ -0,0 +1,10 @@
+// Tipos Primitivos
+let valor: number = 3000;
+let nome: string = "";
+let isPago: boolean = false;
+let qualquer: any = 22;
+qualquer = "";
+
+// Arrays
+const lista = [];
+lista.push("miu", "dog", 22, true, []);
