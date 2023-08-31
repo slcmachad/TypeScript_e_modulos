c6fa192c9dd929fbba198ff0d864012a083a34d0
Salomao <salomao.machado.75@gmail.com>
2023-08-31 20:05:20 -0300

    Modularizando para facilitar o entendimento e manutenção do código.

--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -1,27 +1,18 @@
-import { formatarData, formatarMoeda } from "../utils/formatters.js";
-import { FormatoData } from "../types/FormatoData.js";
-import Conta from "../types/Conta.js";
-
+import { formatarMoeda } from "../utils/formatters";
+import  Conta  from "../types/Conta";
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
-const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 
-if(elementoDataAcesso !== null){
-    elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
-}
-
-renderizarSaldo();
-
-function renderizarSaldo():void{
-    if(elementoSaldo !== null){
-        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo())
+function renderizarSaldo(): void {
+    if (elementoSaldo !== null) {
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
     }
 }
 
 const SaldoComponent = {
-    atualizar(){
-        renderizarSaldo()
-    }
-}
+    atualizar: function () {
+        renderizarSaldo();
+    },
+};
 
-export default SaldoComponent
+export default SaldoComponent;
