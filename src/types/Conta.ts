699c720a826c80927a2ff7748bb0332f552c4acc
Salomao <salomao.machado.75@gmail.com>
2023-09-02 21:20:57 -0300

    criando metodos nescessários

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -35,6 +35,52 @@ export class Conta {
 
         return gruposTransacoes;
     }
+
+    getSaldo() {
+        return this.saldo;
+    }
+
+    getDataAcesso(): Date {
+        return new Date();
+    }
+
+    registrarTransacao(novaTransacao: Transacao): void {
+        if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
+            this.depositar(novaTransacao.valor);
+        } 
+        else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
+            this.debitar(novaTransacao.valor);
+            novaTransacao.valor *= -1;
+        } 
+        else {
+            throw new Error("Tipo de Transação é inválido!");
+        }
+
+        this.transacoes.push(novaTransacao);
+        console.log(this.getGruposTransacoes());
+        localStorage.setItem("transacoes", JSON.stringify(this.transacoes));
+    }
+
+    debitar(valor: number): void {
+        if (valor <= 0) {
+            throw new Error("O valor a ser debitado deve ser maior que zero!");
+        }
+        if (valor > this.saldo) {
+            throw new Error("Saldo insuficiente!");
+        }
+    
+        this.saldo -= valor;
+        localStorage.setItem("saldo", this.saldo.toString());
+    }
+
+    depositar(valor: number): void {
+        if (valor <= 0) {
+            throw new Error("O valor a ser depositado deve ser maior que zero!");
+        }
+    
+        this.saldo += valor;
+        localStorage.setItem("saldo", this.saldo.toString());
+    }
 }
 
 const conta = new Conta("Joana da Silva Olveira");
