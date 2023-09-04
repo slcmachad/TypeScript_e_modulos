3b7893bbe5be4e3cef858263a5bfc6dc11c95ff6
Salomao <salomao.machado.75@gmail.com>
2023-09-03 22:43:12 -0300

    alterando tipo de retorno esperado

--- src/types/Armazenador.ts
+++ src/types/Armazenador.ts
@@ -6,15 +6,15 @@ export class Armazenador{
         localStorage.setItem(chave, valorComoString);
     }
 
-    static obter(chave: string, reviver?: (this: any, key: string, value: any) => any) {
+    static obter<T>(chave: string, reviver?: (this: any, key: string, value: any) => any): T | null {
         const valor = localStorage.getItem(chave);
 
         if(valor === null){
             return null;
         }
         if(reviver){
-            return JSON.parse(valor, reviver);
+            return JSON.parse(valor, reviver) as T;
         }
-        return JSON.parse(valor);
+        return JSON.parse(valor) as T;
     }
 }
