e2fe66bc8abfc580626469ec6a92b6a3af063dbc
Salomao <salomao.machado.75@gmail.com>
2023-09-03 22:59:01 -0300

    inserindo funções de validação

--- dist/js/types/Decorators.js
+++ dist/js/types/Decorators.js
@@ -1,2 +1,13 @@
 export function ValidaDebito(target, propertyKey, descriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (valorDoDebito) {
+        if (valorDoDebito <= 0) {
+            throw new Error("Valor Precisa ser maior que ZERO!");
+        }
+        if (valorDoDebito > this.saldo) {
+            throw new Error("Saldo insuficiente!");
+        }
+        return originalMethod.apply(this, [valorDoDebito]);
+    };
+    return descriptor;
 }
--- src/types/Decorators.ts
+++ src/types/Decorators.ts
@@ -1,3 +1,16 @@
 export function ValidaDebito(target: any,  propertyKey: string, descriptor: PropertyDescriptor){
-    
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function(valorDoDebito: number){
+        if(valorDoDebito <= 0){
+            throw new Error("Valor Precisa ser maior que ZERO!")
+        }
+        if(valorDoDebito > this.saldo){
+            throw new Error("Saldo insuficiente!")
+        }
+
+        return originalMethod.apply(this, [valorDoDebito])
+    }
+
+    return descriptor;
 }
